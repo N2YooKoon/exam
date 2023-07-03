49dc027e17c0515d2d585db997cbf8d27c99950b
N2.YooKoon <N2.YooKoon@192.168.219.105>
2023-07-04 01:36:14 +0900

    브랜치 테스트입니다.

--- src/main/java/git_test01/testClass.java
+++ src/main/java/git_test01/testClass.java
@@ -4,6 +4,10 @@ public class testClass{
 	public static void main(String[] arge) {
 		System.out.println("이것은 깃허브 테스트입니다.");
 		System.out.println("test git hub.");
+		/*
+		 * 브랜치를 따보았다. 이게 뭔 짓인지 모르겠다. 십새꺄 니실력에 잠이 오냐?? 제발 좀 똑바로 살자. 스프링을 알아야지 지금 깃허브
+		 * 만질때냐고
+		 */
 	}
 
 }
